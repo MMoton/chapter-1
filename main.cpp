cfbff70e0ee3e0c08223be00c0cc535a141a3933
MMoton <72563671+MMoton@users.noreply.github.com>
2026-03-30 12:11:09 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  chapter 1
+//
+//  Created by Maya Moton on 3/30/26.
+//
+
+// This program calculates the user's pay.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+double hours, rate, pay;
+
+// Get the number of hours worked.
+cout << "How many hours did you work? ";
+cin >> hours;
+// Get the hourly pay rate.
+cout << "How much do you get paid per hour? ";
+cin >> rate;
+// Calculate the pay.
+pay = hours * rate;
+// Display the pay.
+cout << "You have earned $" << pay << endl;
+return 0;
+}
